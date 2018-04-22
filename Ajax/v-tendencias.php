d2a641eb2c82a6e9575c85fa632332b0f6e6fe16
Rony R <ronyrodriguez2011@hotmail.com>
2018-04-22 15:57:28 -0600

    II Entrega

---
+++ Ajax/v-tendencias.php
@@ -0,0 +1,28 @@
+<?php
+    for($i=0; $i<7; $i++){
+        echo ' <div class="container-fluid">
+        <div class="row no-gutters">
+            <div class="foto col-xl-4 col-lg-4 col-md-4">
+                <a href="watch/video-watch.php" class="d-block">
+                <div class="video-miniatura-tendencias">
+                    <img class="img-fluid" src="img/miniatura.jpg">
+                </div>
+                </a>
+            </div>
+            <div class="info col-xl-8 col-lg-8 col-md-8">
+                <div class="descripcion ml-2 mt-3">
+                    <h4>
+                        <a href="watch/video-watch.php">Titulo del Video</a>
+                    </h4>
+                    <p class="text-muted">Nombre del canal
+                        <br>numero visualizaciones
+                        <br>Tiempo desde que subio el video</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>';
+    }
+
+
+?>
